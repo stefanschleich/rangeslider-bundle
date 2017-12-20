1b6cc3f870b73ae1336a89b5b9a89c805f15737f
Stefan Schleich <stefan@plusx.de>
2017-12-20 20:42:57 +0100

    Remove dummy texts.

--- src/Resources/contao/languages/de/tl_form_field.php
+++ src/Resources/contao/languages/de/tl_form_field.php
@@ -5,7 +5,7 @@
     $GLOBALS['TL_LANG']['tl_form_field']['rsText'] = 'Textwerte';
     $GLOBALS['TL_LANG']['tl_form_field']['rsNumeric'] = 'Numerische Werte';
     $GLOBALS['TL_LANG']['tl_form_field']['rangeSliderTextValues'] = array('Skalenwerte', 'Bezeichnung der einzelnen Skalenwerte als Text.');
-    $GLOBALS['TL_LANG']['tl_form_field']['rangeSliderNumericMin'] = array('Startwert', 'Blo');
-    $GLOBALS['TL_LANG']['tl_form_field']['rangeSliderNumericMax'] = array('Endwert', 'Blo');
-    $GLOBALS['TL_LANG']['tl_form_field']['rangeSliderNumericStep'] = array('Stufe', 'Blo');
-    $GLOBALS['TL_LANG']['tl_form_field']['rangeSliderGrid'] = array('Skala anzeigen', 'Skala mit Zwischenschritten unterhalb des Bedienelements anzeigen.');
+    $GLOBALS['TL_LANG']['tl_form_field']['rangeSliderNumericMin'] = array('Startwert', 'Startwert der Skala (z.B. 0)');
+    $GLOBALS['TL_LANG']['tl_form_field']['rangeSliderNumericMax'] = array('Endwert', 'Endwert der Skala (z.B. 100)');
+    $GLOBALS['TL_LANG']['tl_form_field']['rangeSliderNumericStep'] = array('Abstufung', 'Abstufung der Skala (z.B. 10). Steuert die möglichen Schritte bei Änderung des Schiebereglers.');
+    $GLOBALS['TL_LANG']['tl_form_field']['rangeSliderGrid'] = array('Skala anzeigen', 'Skala mit Zwischenschritten unterhalb des Schiebereglers anzeigen.');
